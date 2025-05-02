52fc3f635a7b09fa11add1a1bf47f5cdbe84cafe
GlazovYuri <glazov.y0@gmail.com>
2025-05-02 14:03:49 +0300

    added comment in main.py to configure state machine

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ if __name__ == "__main__":
 
     PROCESSORS = [
         FieldCreator(
-            debug_mode=True,
-            debug_game_state=const.State.RUN,
+            debug_mode=True,  # True => ignore commands from referee
+            debug_game_state=const.State.RUN,  # for other states (except STOP and HALT) add "debug_active_team" param
+            # debug_active_team=const.Color.ALL,
         ),
         SSLController(
             ally_color=const.COLOR,
